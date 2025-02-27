4e0580d650ecbdb4ad1ce11689e8b1f4eb8c5b88
subhava06 <subhav.22gcebcs190@galgotiacollege.edu>
2025-02-27 06:59:13 +0530

    Armstrong no.

--- OOPS/Methods.java
+++ OOPS/Methods.java
@@ -1,3 +1,26 @@
+import java.util.*;
 public class Methods {
+    public static void main(String[] args) {
+        //Armstrong  no.
+        System.out.println("Enter the no. :" );
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
     
+        System.out.println(armstrong(n));
+        
+    }
+
+    static boolean armstrong(int n)
+    {
+        int original = n;
+        int result = 0;
+        while(n>0)
+        {
+            int rem = n%10;
+            result = result + rem*rem*rem;
+            n = n/10153;
+
+        }
+        return original == result;
+    }
 }
