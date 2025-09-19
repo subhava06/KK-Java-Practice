e198d558823341fa8aca4e0c65d5a7714e000746
SUBHAVA OJHA <subhava.22gcebcs190@galgotiacollege.edu>
2025-09-19 07:23:49 +0530

    func overloading

--- Strings and StringBuilder/Basic.java
+++ Strings and StringBuilder/Basic.java
@@ -1,13 +1,19 @@
+import java.util.Arrays;
+
 public class Basic {
     public static void main(String[] args) {
         String name = "Subhava Ojha";
-        System.out.println(name);
+     //   System.out.println(name);
 
         String b = name;
-        System.out.println(name == b);
+      //  System.out.println(name == b);
 
-        String c = new String("Subhava Ojha");
-        System.out.println(c==b);
-        System.out.println(c.equals(b));
+       // String c = new String("Subhava Ojha");
+       // System.out.println(c==b);
+       // System.out.println(c.equals(b));
+
+       System.out.println(Arrays.toString(new int[]{1,2,3,4}));
+
+
     }
 }
