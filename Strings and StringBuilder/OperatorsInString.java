6188a82781853624796a2d06e185a3f6d34c7b35
SUBHAVA OJHA <subhava.22gcebcs190@galgotiacollege.edu>
2025-09-19 09:44:35 +0530

    operators done in string

---
+++ Strings and StringBuilder/OperatorsInString.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class OperatorsInString {
+
+    public static void main(String[] args) {
+        System.out.println('a' + 'b');
+        System.out.println("a" +"b");
+        System.out.println('a' + 3);
+        System.out.println((char)('a' + 3));
+
+        System.out.println("a" + 1);
+
+        System.out.println("Subhava" + new ArrayList<>());
+
+        System.out.println(new Integer(56) + "" + new ArrayList<>());
+    }
+    
+}
