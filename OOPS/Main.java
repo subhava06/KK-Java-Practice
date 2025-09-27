3115c15c9847932ac9efdba5d9f4ea460f74add7
SUBHAVA OJHA <subhava.22gcebcs190@galgotiacollege.edu>
2025-09-27 12:54:30 +0530

    using . operator

--- OOPS/Main.java
+++ OOPS/Main.java
@@ -4,9 +4,15 @@ public class Main {
     public static void main(String[] args) {
         Student subhava;
         subhava = new Student();
-        Student[] students = new Student[5];
-        
-        System.out.println(Arrays.toString(students));
+        subhava.rollno= 1;
+        subhava.name= "Subhava Ojha";
+       subhava.marks = 87.6f;
+        System.out.println(subhava.rollno);
+        System.out.println(subhava.name);
+        System.out.println(subhava.marks);
+
+       // Student[] students = new Student[5];
+       // System.out.println(Arrays.toString(students));
     }
 }
 
@@ -14,5 +20,5 @@ class Student {
 
     int rollno ;
     String name;
-    float marks; 
+    float marks = 90.0f; 
 }
