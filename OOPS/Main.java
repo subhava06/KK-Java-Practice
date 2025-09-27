ddb48800af07be88e7b87b071af54ded00714d59
SUBHAVA OJHA <subhava.22gcebcs190@galgotiacollege.edu>
2025-09-27 12:49:11 +0530

    dynamic mem allocn to class

--- OOPS/Main.java
+++ OOPS/Main.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Student subhava;
+        subhava = new Student();
         Student[] students = new Student[5];
         
         System.out.println(Arrays.toString(students));
