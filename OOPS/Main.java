79b1b82a06de68874a6fcf9e1905ae6c61c29ef1
SUBHAVA OJHA <subhava.22gcebcs190@galgotiacollege.edu>
2025-09-27 13:13:29 +0530

    importance of this keyword

--- OOPS/Main.java
+++ OOPS/Main.java
@@ -2,11 +2,10 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Student subhava;
-        subhava = new Student();
-        subhava.rollno= 1;
-        subhava.name= "Subhava Ojha";
-       subhava.marks = 87.6f;
+        Student subhava= new Student();
+
+        
+
         System.out.println(subhava.rollno);
         System.out.println(subhava.name);
         System.out.println(subhava.marks);
@@ -21,4 +20,17 @@ class Student {
     int rollno ;
     String name;
     float marks = 90.0f; 
+
+    //we need a way to add the values of above properties object by object
+
+    //we need one word to access every object
+
+    //constructor
+    Student() {
+     this.rollno= 13;
+      this.name= "Subhava Ojha";
+       this.marks = 87.6f;
+
+       // the above "this" word will be replaced with the object name that we use
+    }
 }
