4db2d57a8478239af5e223bdee42152eaf4554fe
SUBHAVA OJHA <subhava.22gcebcs190@galgotiacollege.edu>
2025-09-27 14:50:51 +0530

    oop1 done

--- OOPS/WrapperExample.java
+++ OOPS/WrapperExample.java
@@ -13,9 +13,20 @@ public class WrapperExample {
         //instead use Integer object, then it will pass and swap
         Integer p = 20;
         Integer q = 30;
-        System.out.println("Now it will swap");
+        System.out.println("Now also it wont swap");
         swap(p,q);
         System.out.println(p+" "+q);
+
+        final A subhava = new A("Subhava");
+        subhava.name = "other name"; // this is allowed with final objects
+        //but below is not allowed
+        // when a non primitive is final, you cannot reassign it
+        //subhava = new A("new object");
+        
+        A obj;
+        for(int i=0; i< 100000000000; i++){
+            obj = new A("random name");
+        }
 
 
     }
@@ -30,5 +41,16 @@ public class WrapperExample {
 public class A {
     final int a = 2;
     // always initialize a final var while declaring it.
+    String name;
+
+    public A(String name) {
+        this.name = name;
+    }
+
+   @Override
+   protected void finalize() throws Throwable {
+       // TODO Auto-generated method stub
+       System.out.println("Object is destroyed");
+   }
 
 }
