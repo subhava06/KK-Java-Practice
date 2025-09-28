9ac6552039aeba08d7a081e8a27ad0af3792f323
SUBHAVA OJHA <subhava.22gcebcs190@galgotiacollege.edu>
2025-09-28 10:43:20 +0530

    packages started

--- OOPS/WrapperExample.java
+++ OOPS/WrapperExample.java
@@ -6,36 +6,28 @@ public class WrapperExample {
         int b= 20;
 
 
-        Integer num=45;
-        swap(10,20);
+       // Integer num=45;
+       // swap(10,20);
         System.out.println("It won't swap as in java only values are passed");
         System.out.println(a+" "+b);
         //instead use Integer object, then it will pass and swap
         Integer p = 20;
         Integer q = 30;
         System.out.println("Now also it wont swap");
-        swap(p,q);
+        //swap(p,q);
         System.out.println(p+" "+q);
 
-        final A subhava = new A("Subhava");
+        A subhava = new A("Subhava");
         subhava.name = "other name"; // this is allowed with final objects
         //but below is not allowed
         // when a non primitive is final, you cannot reassign it
         //subhava = new A("new object");
         
-        A obj;
-        for(int i=0; i< 100000000000; i++){
-            obj = new A("random name");
-        }
+        
 
 
     }
-    public static void swap(Integer a , Integer b)
-    {
-        int temp =a;
-        a = b;
-        b = temp;
-    }
+   
 }
 
 public class A {
@@ -47,10 +39,10 @@ public class A {
         this.name = name;
     }
 
-   @Override
-   protected void finalize() throws Throwable {
-       // TODO Auto-generated method stub
-       System.out.println("Object is destroyed");
-   }
+//    @Override
+//    protected void finalize() throws Throwable {
+//        // TODO Auto-generated method stub
+//        System.out.println("Object is destroyed");
+//    }
 
 }
