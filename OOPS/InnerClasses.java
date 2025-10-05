364a6a8941f09a72bfabdba550ef75f89eb0c981
SUBHAVA OJHA <subhava.22gcebcs190@galgotiacollege.edu>
2025-10-05 13:45:15 +0530

    Innerclasses concept

---
+++ OOPS/InnerClasses.java
@@ -0,0 +1,9 @@
+// eg to show classes inside a class
+
+public class InnerClasses {
+    
+   static class Test { // only inner classes can be static not outer one
+
+    }
+
+}
--- OOPS/StaticBlock.java
+++ OOPS/StaticBlock.java
@@ -4,6 +4,8 @@ public class StaticBlock {
     static int a = 4;
     static int b;
 
+
+    //will only run once, when the first object is created,i.e, when the class is loaded for the first time
      static {
         System.out.println("I am in static block");
         b = a*5;
@@ -13,5 +15,11 @@ public class StaticBlock {
         
         StaticBlock obj = new StaticBlock();
         System.out.println(StaticBlock.a + " " + StaticBlock.b);
+
+        StaticBlock.b +=3;
+        System.out.println(StaticBlock.a + " " + StaticBlock.b);
+
+        StaticBlock obj2 = new StaticBlock();
+        
     }
 }
