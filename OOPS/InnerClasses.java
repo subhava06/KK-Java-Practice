501bf3416545e9f0d55b00b41b974df267636b16
SUBHAVA OJHA <subhava.22gcebcs190@galgotiacollege.edu>
2025-10-05 13:58:50 +0530

    Innerclasses complete

--- OOPS/InnerClasses.java
+++ OOPS/InnerClasses.java
@@ -1,9 +1,37 @@
 // eg to show classes inside a class
 
-public class InnerClasses {
-    
-   static class Test { // only inner classes can be static not outer one
 
+ 
+public class InnerClasses {
+
+    // now the class Test does not depend upon the objects of the outer class
+    static class Test { // only inner classes can be static not outer one
+
+        String name;
+
+        public Test(String name)
+        {
+            this.name = name;
+        }
+    }
+    
+    
+
+        public static void main(String[] args) {
+            Test a = new Test("Kunal");
+            Test b = new Test("Rahul");
+        
+            System.out.println(a.name);
+            System.out.println(b.name);
+        }
     }
 
-}
+
+
+// static class A {
+
+// }
+
+// note: Static level stuff are Class level stuff
+// they are initialized at compile time
+//unlike objects that are created at compile time
