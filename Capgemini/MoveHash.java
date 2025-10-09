1e22410acbbd1a94fc93ae41fe0a62016c5e4c00
SUBHAVA OJHA <subhava.22gcebcs190@galgotiacollege.edu>
2025-10-09 14:51:50 +0530

    Capgemini coding quesn

---
+++ Capgemini/MoveHash.java
@@ -0,0 +1,10 @@
+
+
+public class MoveHash {
+    public static void main(String[] args) {
+        
+        String input = "Move#Hash#to#Front";
+        char ch = '9';
+        System.out.println(ch-'0');
+    }
+}
