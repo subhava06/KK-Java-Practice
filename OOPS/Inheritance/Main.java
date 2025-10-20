587fe1b2ea442cb3cf5814eb99dec735e5cb7d24
SUBHAVA OJHA <subhava.22gcebcs190@galgotiacollege.edu>
2025-10-20 16:39:18 +0530

    Inheritance begins

---
+++ OOPS/Inheritance/Main.java
@@ -0,0 +1,11 @@
+
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Box  box = new Box();
+
+        System.out.println(box.l+" "+box.h+" "+box.w);
+    }
+}
