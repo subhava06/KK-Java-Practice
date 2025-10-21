99a46d2ccad9c9cd5d0cc844ccfe45b1ae0fbb9c
SUBHAVA OJHA <subhava.22gcebcs190@galgotiacollege.edu>
2025-10-21 16:41:20 +0530

    diff in referencing

--- OOPS/Inheritance/BoxWeight.java
+++ OOPS/Inheritance/BoxWeight.java
@@ -10,8 +10,12 @@ public class BoxWeight extends Box {
 
     public BoxWeight(double h, double l, double w, double weight) {
         super(l,h,w); //what is this? call the parent class constructor
-        //used to initialize the values present in parent class
+        //used to initialize the values present in parent class 
+        // but not the private ones
 
         this.weight = weight;
     }
+
+
+    
 }
--- OOPS/Inheritance/Main.java
+++ OOPS/Inheritance/Main.java
@@ -16,5 +16,13 @@ public class Main {
 
         BoxWeight box4 = new BoxWeight(1.2,3.44,5.33,22.5);
         System.out.println(box4.h+" "+box4.w+" "+box4.weight+" "+box4.l);
+
+
+        // NOTE: it is the type of the reference var(Box box5) and not the object(BoxWeight())
+    // that determines what members can be accessed
+    Box box5 = new BoxWeight(2, 5, 6, 7);
+    System.out.println(box5.h+" "+box5.l+" "+box5.w+" ");
+
+   // BoxWeight box6 = new Box(2,4,6); // this gives an error because we are trying to reference a child to a parent
     }
 }
