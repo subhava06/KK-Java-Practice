25883a60fb30acfd8728495ef88be27654d9eec9
SUBHAVA OJHA <subhava.22gcebcs190@galgotiacollege.edu>
2025-10-29 03:07:23 +0530

    swap using bit manipulation

--- bit manipulation/BinConversion.java
+++ bit manipulation/BinConversion.java
@@ -4,7 +4,15 @@ public class BinConversion {
 
     public static void main(String[] args) {
         
-        
+
+        //swap two numbers
+        int a = 5, b = 6;
+        System.out.println("before swap: "+a+" "+b);
+        a = a^b;
+        b = a^b;
+        a = a^b;
+        System.out.println("After swap: "+a+" "+b);
+    
     }
     
 }
