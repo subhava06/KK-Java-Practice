f120cd1d483095099a0a7207c8d6779cb945505c
SUBHAVA OJHA <subhava.22gcebcs190@galgotiacollege.edu>
2025-11-06 03:39:34 +0530

    TS contains elems in sorted order

--- Set DS/RangeQueries.java
+++ Set DS/RangeQueries.java
@@ -1,9 +1,18 @@
 //range query means when you have to fetch elements in a given range
 // Implemented using TreeSet
 
+import java.util.TreeSet;
+
 public class RangeQueries {
     
     public static void main(String[] args) {
+        
+        TreeSet<Integer> set = new TreeSet<>();
+        
+        set.add(20);
+        set.add(10);
+        set.add(23);
+        System.out.println(set); // prints in sorted order
         
     }
 }
