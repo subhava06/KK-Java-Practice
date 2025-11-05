7e1b828a4d08a25a77e59ba13d9607a96b078095
SUBHAVA OJHA <subhava.22gcebcs190@galgotiacollege.edu>
2025-11-06 03:35:27 +0530

    understanding treeset using range queries

---
+++ Set DS/RangeQueries.java
@@ -0,0 +1,9 @@
+//range query means when you have to fetch elements in a given range
+// Implemented using TreeSet
+
+public class RangeQueries {
+    
+    public static void main(String[] args) {
+        
+    }
+}
